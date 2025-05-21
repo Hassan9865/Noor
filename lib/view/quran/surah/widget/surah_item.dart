cb1f28c49aa04d3a823a3cef4e845e386264bcbf
Hassan9865 <syedhassanraza0311@gmail.com>
2025-05-21 20:05:03 +0500

    Styling

--- lib/view/quran/surah/widget/surah_item.dart
+++ lib/view/quran/surah/widget/surah_item.dart
@@ -10,12 +10,37 @@ class SurahItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(child: Text(surah.id.toString())),
-      title: Text(surah.transliteration),
-      subtitle: Text('${surah.translation} - ${surah.totalVerses} verses'),
-      trailing: Text(surah.name, style: const TextStyle(fontSize: 20)),
-      onTap: onTap,
+    final Txtsize = MediaQuery.of(context).size.width;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: ListTile(
+          tileColor: Colors.teal[50]!,
+          leading: CircleAvatar(child: Text(surah.id.toString())),
+          title: Text(
+            surah.transliteration,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+          ),
+          subtitle: Text(
+            '${surah.translation} - ${surah.totalVerses} verses',
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          trailing: Text(
+            surah.name,
+            style: TextStyle(
+              fontSize: Txtsize * 0.06,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          onTap: onTap,
+        ),
+      ),
     );
   }
 }
